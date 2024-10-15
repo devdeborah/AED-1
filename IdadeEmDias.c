c0b29d1a134995e3eae0cce81a125118ce864f66
Deborah <146981071+devdeborah@users.noreply.github.com>
2024-10-15 11:38:01 -0300

    Create IdadeEmDias.c

---
+++ IdadeEmDias.c
@@ -0,0 +1,14 @@
+//Retorna o cálculo simples de uma idade (recebida em dias inteiros) subdividido em anos, meses e dias.
+#include <stdio.h>
+
+int main(void) {
+    int id, a, m, d;
+    scanf("%d", &id);
+    a = id/365;
+    int ra = id % 365;
+    m = ra/30;
+    int rm = ra % 30;
+    d = rm;
+    printf ("%d ano(s)\n%d mes(es)\n%d dia(s)\n", a, m, d);
+    return 0;
+}
